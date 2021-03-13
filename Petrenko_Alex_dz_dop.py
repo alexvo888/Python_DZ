0849ace1c7241eca2c88784ff2fa1082d27ff192
alexvo888 <79628473+alexvo888@users.noreply.github.com>
2021-03-13 14:51:22 +0300

    Delete Petrenko_Alex_dz_dop.py

--- Petrenko_Alex_dz_dop.py
+++
@@ -1,8 +0,0 @@
-test_list = ['Hello', 42.00000014, True, [1, 2, 3], 1, 2, 'Basil']
-for i in test_list:
-    if i == [1, 2, 3]:
-        del test_list[3]
-        test_list.insert(3, 1)
-        test_list.insert(4, 2)
-        test_list.insert(5, 3)
-print(test_list)
